768e78c1ea4dee543b6d80c69026aca93637666c
Vansh <86408597+vansh1505@users.noreply.github.com>
2026-01-03 16:56:36 +0530

    Time: 5 ms (31.79%), Space: 43.2 MB (30.26%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
@@ -0,0 +1,17 @@
+class Solution {
+    final static int MOD = 1_000_000_007;
+
+    public int numOfWays(int n) {
+        long[] a = new long[n];
+        long[] b = new long[n];
+
+        a[0] = 6;
+        b[0] = 6;
+
+        for (int i = 1; i < n; i++) {
+            a[i] = (2 * a[i - 1] + 2 * b[i - 1]) % MOD;
+            b[i] = (2 * a[i - 1] + 3 * b[i - 1]) % MOD;
+        }
+        return (int)((a[n - 1] + b[n - 1]) % MOD);
+    }
+}
